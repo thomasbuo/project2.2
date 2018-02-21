834f4f35d1c35184816016a8cf0033358af4253d
vincevandriel <vincevandriel@gmail.com>
2018-02-21 11:46:52 +0100

    Car Object added

--- project2.2/src/main/java/objects/Car.java
+++ project2.2/src/main/java/objects/Car.java
@@ -1,5 +1,16 @@
 package objects;
 
-public class Car {
+import java.awt.Point;
 
+public class Car {
+	public Point origin;
+	public Point destination;
+	public int departureTime;
+	
+	//Constructor
+	public Car(Point origin, Point destination, int departureTime) {
+		this.origin = origin;
+		this.destination = destination;
+		this.departureTime = departureTime;
+	}
 }
