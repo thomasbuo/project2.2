bdf4f0ccf66e40697f610061b21dc1a99b154828
vincevandriel <vincevandriel@gmail.com>
2018-02-21 12:56:49 +0100

    Car update

--- project2.2/src/main/java/objects/Car.java
+++ project2.2/src/main/java/objects/Car.java
@@ -1,16 +1,19 @@
 package objects;
 
 import java.awt.Point;
+import java.util.Vector;
 
 public class Car {
 	public Point origin;
 	public Point destination;
+	public Vector currentLocation;
 	public int departureTime;
 	
 	//Constructor
-	public Car(Point origin, Point destination, int departureTime) {
+	public Car(Point origin, Point destination, Vector currentLocation, int departureTime) {
 		this.origin = origin;
 		this.destination = destination;
+		this.currentLocation = currentLocation;
 		this.departureTime = departureTime;
 	}
 }
