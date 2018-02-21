5900adae79f2554109da1fd9a162214dfb559b8d
thomasbuo <thomasbuo@gmail.com>
2018-02-21 14:17:01 +0100

    interface

--- project2.2/src/main/java/gui/GraphicalInterface.java
+++ project2.2/src/main/java/gui/GraphicalInterface.java
@@ -20,9 +20,7 @@ public class GraphicalInterface extends JFrame {
 	private JPanel contentPane;
 	private final Action action = new SwingAction();
 
-	/**
-	 * Launch the application.
-	 */
+	
 
 	/**
 	 * Create the frame.
@@ -40,9 +38,9 @@ public class GraphicalInterface extends JFrame {
 		contentPane.add(panel);
 		panel.setLayout(null);
 		
-		JButton btnStart = new JButton("Start");
-		btnStart.setBounds(10, 11, 164, 33);
-		panel.add(btnStart);
+		JButton start_button = new JButton("Start");
+		start_button.setBounds(10, 11, 164, 33);
+		panel.add(start_button);
 		
 		JTabbedPane tabbedPane = new JTabbedPane(JTabbedPane.TOP);
 		tabbedPane.setBounds(10, 128, 164, 352);
@@ -60,18 +58,25 @@ public class GraphicalInterface extends JFrame {
 		JPanel panel_5 = new JPanel();
 		tabbedPane.addTab("Signs", null, panel_5, null);
 		
-		JButton load_button = new JButton("Load");
-		load_button.setBounds(10, 55, 79, 23);
-		panel.add(load_button);
-		load_button.addActionListener(new LoadButton());
+		SaveButton sb = new SaveButton();
+		//LoadButton lb = new LoadButton();
 		
 		JButton save_button = new JButton("Save");
 		save_button.setBounds(95, 55, 79, 23);
 		panel.add(save_button);
+		save_button.addActionListener(sb);
+		
+		JButton load_button = new JButton("Load");
+		load_button.setBounds(10, 55, 79, 23);
+		panel.add(load_button);
+		//load_button.addActionListener(lb);
+		
+		
 		
 		JButton reset_button = new JButton("Reset");
 		reset_button.setBounds(49, 94, 89, 23);
 		panel.add(reset_button);
+		//reset_button.addActionListener(lb);
 		
 		JPanel panel_1 = new JPanel();
 		panel_1.setBounds(10, 22, 830, 491);
@@ -82,6 +87,7 @@ public class GraphicalInterface extends JFrame {
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
+			System.out.println("Loading");
 		JFrame frame2 = new JFrame("loading");
 		frame2.setVisible(true);
 		frame2.setSize(200, 200);
@@ -97,7 +103,14 @@ public class GraphicalInterface extends JFrame {
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			//do something
+			System.out.println("saving");
+		JFrame frame3 = new JFrame("saving");
+		frame3.setVisible(true);
+		frame3.setSize(200, 200);
+		JLabel label1 = new JLabel("saving");
+		JPanel panel1 = new JPanel();
+		frame3.add(panel1);
+		panel1.add(label1);
 			
 		}
 		
@@ -106,11 +119,19 @@ public class GraphicalInterface extends JFrame {
 
 		@Override
 		public void actionPerformed(ActionEvent arg0) {
-			//do something
+			System.out.println("resettin");
+			JFrame frame4 = new JFrame("resetting");
+		frame4.setVisible(true);
+		frame4.setSize(200, 200);
+		JLabel label2 = new JLabel("resetting");
+		JPanel panel2 = new JPanel();
+		frame4.add(panel2);
+		panel2.add(label2);
 			
 		}
 		
 	}
+	
 
 	private class SwingAction extends AbstractAction {
 		public SwingAction() {
