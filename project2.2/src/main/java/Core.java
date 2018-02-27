0768f71becc0fe6e2cb193f0ee1da1cce74c99e2
weidler <admin@tonioweidler.de>
2018-02-27 20:17:29 +0100

    Added basic StreetMap Object and methods. Added Classes for Connections and Intersections, but without Functionality

--- project2.2/src/main/java/Core.java
+++ project2.2/src/main/java/Core.java
@@ -1,11 +1,25 @@
 import gui.GraphicalInterface;
 
+import java.util.ArrayList;
+
+import datastructures.*;
+
 public class Core {
 
 	public static void main(String[] args){
 		
 		GraphicalInterface graphical_interface = new GraphicalInterface();
 		graphical_interface.setVisible(true);
+	
+		StreetMap map = new StreetMap();
 		
+		map.addIntersection(new Intersection());
+		map.addIntersection(new Intersection());
+		map.addIntersection(new Intersection());
+
+		map.addConnection(2, 1, new Connection());
+		
+		System.out.println(map.getAdjacencyMatrix());
 	}
 }
+
